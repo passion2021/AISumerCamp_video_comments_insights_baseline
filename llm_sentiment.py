bd98d1b1dc672e4fb3af8766193e09403d156643
lmx <1213640671@qq.com>
2025-07-12 17:42:08 +0800

    feat:情绪分类脚本

--- llm_sentiment.py
+++ llm_sentiment.py
@@ -1,4 +1,8 @@
+import json
+
 import pandas as pd
+from libs.easy_llm import *
+from libs.easy_llm.utils.extract_tools import json_parse_dirty
 
 df_comments = pd.read_csv('./origin_comments_data.csv')
 prompt = '''
@@ -10,9 +14,10 @@ prompt = '''
 3. 情感混合判断：同时出现褒贬词（如"质量好但价格高"）选3
 
 评论样本：{}
-
+'''
+output = '''
 输出限制：
-严格按照格式返回JSON。
+无需输出任何前缀后缀，以及任何解释。严格按照格式返回JSON。
 {
   "sentiment": ["1-正面","2-负面","3-正负混合","4-中性","5-不相关"],
   "has_scenario": [0/1],
@@ -20,6 +25,11 @@ prompt = '''
   "has_suggestion": [0/1]
 }
 '''
-print(df_comments.columns)
+# print(df_comments.columns)
 for index, row in df_comments.iterrows():
-    print(index, row.tolist())
+    messages = MsgList(HumanMsg(prompt.format(row['comment_text']) + output))
+    llm_resp = ''
+    for chunk in qwen2_5_14b.stream(messages.to_json()):
+        llm_resp += chunk
+    result = json_parse_dirty(llm_resp)
+    print(f"index: {index}, Comment ID: {row['comment_id']}, Comment Text: {row['comment_text']}, Result: {result}")
