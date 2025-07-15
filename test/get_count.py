d7f47bb54efb1a62902e3aa3f7bfe2966052cb22
lmx <1213640671@qq.com>
2025-07-15 16:53:30 +0800

    chore: 清理无用代码

--- test/get_count.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from db.schema import Sentiment
-old_df = pd.read_csv('../data/copy/submit_comments.csv')
-
-
-for i,row in old_df.iterrows():
-    print(i, row['comment_id'])
-
-# 一共6476个数据
